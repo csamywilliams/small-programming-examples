c3889fe46c292c4d92b5ec3a271b4b5d5ada2e72
Amy Williams <csamywilliams@gmail.com>
2017-05-15 20:05:52 +0100

    Bottles of beer code

---
+++ Bottles of beer/bottlesOfBeer.js
@@ -0,0 +1,27 @@
+﻿var beerBottles = 99;
+
+function formatLines(bottle) {
+  var songLine = '<p> {0} bottles of beer on the wall <br> ' +
+    '{0} bottles of beer <br>' +
+    'Take one down, pass it around <br>' +
+    '{1} bottles of beer on the wall' +
+    '</p>';
+
+  songLine = songLine.replace(new RegExp('\\{0\\}', 'g'), bottle);
+  songLine = songLine.replace('{1}', bottle - 1);
+
+  if (bottle === 1) {
+    songLine = songLine.replace(new RegExp('1 bottles', 'g'), '1 bottle');
+  }
+
+  return songLine;
+}
+
+var container = document.getElementById('songContainer');
+var lines = '';
+
+for (var bi = beerBottles; i > 0; i--) {
+  lines += formatLines(i);
+}
+
+container.innerHTML = lines;
